189b8f5b5034d84266680f53f33aa2de85928084
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-07 14:02:48 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
 
-// write your code here
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+const populations = [...document.querySelectorAll('.population')].map(
+  (el) => el.textContent,
+);
+
+console.log(populations);
+
+const sum = populations.reduce((acc, el) => {
+  const num = Number(el.replaceAll(',', ''));
+
+  if (!isNaN(num)) {
+    return acc + num;
+  }
+
+  return acc;
+}, 0);
+
+const average = Math.floor(sum / populations.length);
+
+totalPopulation.textContent = sum.toLocaleString('en-US');
+averagePopulation.textContent = average.toLocaleString('en-US');
